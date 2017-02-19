1b8c5b45ec3efa08dc04d84828cc18d06ce719fa
JohnEfth <eft.giannis@gmail.com>
2017-02-19 15:48:27 +0200

    Merged Nicely

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -1,16 +1,40 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
+#include <cstdio>
+#include <iostream>
 using namespace std;
 
 #define ROWS 50
 #define COLUMNS 50
 
-int main()
-{
-	
-	char pizza[ROWS,COLUMNS];
+
+
+int main(){
+
+	int nrows, ncols, l, h;
+
 	char less_ingridient;				
 	int L,Lcounter_Tomato=0,Lcounter_Mushroom=0;
+
+	/* read input data and allocate memory for pizza_array */
+	scanf("%d", &nrows);
+	scanf("%d", &ncols);
+	scanf("%d", &l);
+	scanf("%d", &h);
+
+	/* allocate memory */
+	char **pizza = new char*[nrows];
+	for(int i = 0; i < nrows; i++){
+		pizza[i] = new char[ncols];
+	} 
+
+	/* read pizza_array */
+	for(int i = 0; i < nrows; i++){
+		for(int j = 0; j < ncols; j++){
+			scanf(" %c", &pizza[i][j]);
+		}
+	}
+	/* *************************************************** */
 
 	for (int i = 0; i < COLUMNS; i++)
 	{
@@ -33,7 +57,7 @@ int main()
 					{
 						if(Lcounter_Tomato < L)
 						{
-							
+							cout<<"Geia sou Giorgo"<<endl;
 						}
 					}
 					else
@@ -59,39 +83,14 @@ int main()
 			}
 		}
 	}
-
-	return 0;
-=======
-#include <cstdio>
-#include <iostream>
-
-using namespace std;
-
-int main(){
-
-	int nrows, ncols, l, h;
-
-	/* read input data and allocate memory for pizza_array */
-	scanf("%d", &nrows);
-	scanf("%d", &ncols);
-	scanf("%d", &l);
-	scanf("%d", &h);
-
-	/* allocate memory */
-	char **pizza = new char*[nrows];
-	for(int i = 0; i < nrows; i++){
-		pizza[i] = new char[ncols];
-	} 
-
-	/* read pizza_array */
-	for(int i = 0; i < nrows; i++){
-		for(int j = 0; j < ncols; j++){
-			scanf(" %c", &pizza[i][j]);
-		}
-	}
-	/* *************************************************** */
+	
+	/* free memory */
+ 	for(int i = 0; i < nrows; i++){
+ 		delete[] pizza[i];
+ 	}
+ 	delete[] pizza;
 
 	return 0;
 
->>>>>>> a472f563cfca983e3b7f90d9ddfad86dea7d6054
+
 }
