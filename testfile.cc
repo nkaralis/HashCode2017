1c17c4d0ca763a1d54f70b8f8c04d166239ba168
george-papado <mye-mail>
2017-02-19 14:10:04 +0200

    test commit

---
+++ testfile.cc
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : testfile.cpp
+// Author      : Moi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+	cout << "!!!Hello hash!!!" << endl; // prints
+	return 0;
+	
+}
